a1aa2914d2680a1657c962b563e455429db73d0a
guojiaxin1994 <511458235@qq.com>
2025-07-01 13:26:27 +0800

    Delete guide.js

--- guide.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        if (window.lucide) {
-            lucide.createIcons();
-        } else {
-            console.error('Lucide library not found.');
-        }
-    }, 100);
-});
